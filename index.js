0b8c8bf6d4d6be975d6fadcd2f66254308bb2f8d
ArundhotiKar <arundhotikar60@gmail.com>
2025-12-04 12:30:07 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const port =5000;
+
+
+const app = express();
+app.use(cors());
+
+
+const uri = "mongodb+srv://pawmart:RgrxZ1ELaW1tNV9y@cluster0.5ejbswi.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    await client.connect();
+
+    app.post('/addlistings', async(req, res) => {
+      
+    })
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from server side - PawMart');
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port: ${port}`);
+})
+
+
